bdbd1b6b5b3584355bdc4b7642b4ac29bf4355dc
ko-shin-ryo <endoifrina@gmail.com>
2023-01-31 23:10:49 +0900

    modify get_wikipedia_ja_link.js

--- get_wikipedia_ja_link.js
+++ get_wikipedia_ja_link.js
@@ -1,12 +1,10 @@
 async function get_wikipedia_ja_link (word) {
-    const url = "https://ja.wikipedia.org/wiki/" + encodeURI(word);
+    const url = "https://ja.wikipedia.org/wiki/" + encodeURIComponent(word);
     try {
         const res = await request(url);
-        const doc = new DOMParser().parseFromString(res, "text/html");
-        const elms = doc.getElementsByTagName("title");
+        const elms = new DOMParser().parseFromString(res, "text/html").getElementsByTagName("title");
         return "[" + elms[0].textContent + "](" + url + ")";
     } catch (e) {
-        console.log(e);
         return "";
     }
 }
