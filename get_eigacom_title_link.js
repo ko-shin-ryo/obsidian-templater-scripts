cc2aee3ac80d5b025238fb5d11578d79fbb7c2c6
ko-shin-ryo <endoifrina@gmail.com>
2025-01-12 11:17:13 +0900

    映画.comリンク取得スクリプトを追加

---
+++ get_eigacom_title_link.js
@@ -0,0 +1,55 @@
+/**
+ * タイトルで検索して映画.comの該当作ページへのMarkdownリンク文字列を生成する
+ * @param {string} title 検索対象タイトル名
+ * @returns {string} 映画.comの紹介ページへのMarkdownリンク文字列
+ */
+async function get_eigacom_title_link (title) {
+    if (!title) return "";
+    // 映画.com検索結果から対象作品のIDを取得する
+    const title_body = extractQuoted(title)
+    const url = "https://eiga.com/search/" + encodeURIComponent(title_body) + "/movie/";
+    let res;
+    try {
+        res = await request(url);
+    } catch (e) {
+        return "";
+    }
+    const elms = new DOMParser().parseFromString(res, "text/html").getElementsByClassName("col-s-3");
+    //console.log(elms);
+    for (const elm of elms) {
+        const movie_title = elm.querySelector('.col-s-3 .title').textContent.trim();
+        const movie_id = elm.querySelector('.col-s-3 a').getAttribute('href').match(/\/movie\/(\d+)/)?.[1];
+        if (movie_title == title_body) {
+            return create_markdown_link(movie_title, movie_id);
+        }
+    }
+    return "";
+}
+
+/**
+ * 鉤括弧または二重鉤括弧の中身を抽出
+ * @param {string} str 文字列
+ * @returns 
+ */
+function extractQuoted(str) {
+    // 鉤括弧「」または二重鉤括弧『』の中身を抽出する正規表現
+    const match = str.match(/(?:「([^」]*)」|『([^』]*)』)/);
+    if (match) {
+        // 最初の捕捉グループまたは2番目の捕捉グループを返す
+        return match[1] || match[2];
+    }
+    return str;
+}
+
+/**
+ * Markdownリンクを作成
+ * @param {string} movie_title 映画タイトル
+ * @param {string} movie_id 映画.com内ID
+ * @returns {string} 映画.comの紹介ページへのMarkdownリンク文字列
+ */
+function create_markdown_link(movie_title, movie_id) {
+    const title_sub = " : 作品情報 - 映画.com"
+    return "[" + movie_title + title_sub + "](https://eiga.com/movie/" + movie_id + ")";
+}
+
+module.exports = get_eigacom_title_link;
